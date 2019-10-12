bdc7a09a54b604dcb3893888facdab924d71a4c0
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-12 16:03:44 -0700

    declaring class and function prototypes

--- main.js
+++ main.js
@@ -1,36 +1,55 @@
 
 
-// Constructor function 
+//  prototype function Constructor function 
 
 function Person(firstName, lastName, dob){
 
     this.firstName = firstName;
     this.lastName = lastName; 
     this.dob = new Date(dob);
-    this.getBirthYear = function(){
+}
+
+Person.prototype.getBirthYear = function(){
+
+    return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function() {
+
+    return `${this.firstName} ${this.lastName}`;
+}
+
+
+//Class 
+
+class Person{
+
+    constructor(firstName, lastName, dob){
+
+    this.firstName = firstName;
+    this.lastName = lastName; 
+    this.dob = new Date(dob);
+    }
+
+    getBirthYear() {
 
         return this.dob.getFullYear();
     }
+    
+    getFullName() {
 
-    this.getFullName = function(){
-
-       // return this.firstName +" "+this.lastName;
-
-       return `${this.firstName} ${this.lastName}`
+        return `${this.firstName} ${this.lastName}`;
     }
-
-
 }
-
 //Instantiate Object 
 
 const person1 = new Person('SaiVarun', 'Tangalla', '6-6-1996');
 const person2 = new Person('Shravya', 'Tangalla', '9-9-1999');
 
 
-console.log(person1.getBirthYear());
+console.log(person1);
 
-console.log(person1.getFullName());
+console.log(person2.getFullName());
 
 
 
