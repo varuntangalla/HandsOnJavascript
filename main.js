6352ab2f77dc42686d7ea0d2556aa5db4ff52aab
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-12 15:05:07 -0700

    switch

--- main.js
+++ main.js
@@ -126,12 +126,27 @@ if( x > 5 )
 */
 
 
-const x = 12;
+const x = 9;
 
 const color = x > 10 ? 'red' : 'blue';
 
 console.log(color);
 
+ 
+switch(color)
+{
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+
+    default: 
+        console.log('color is not red or blue');
+        break;
+
+}
 
 
 
