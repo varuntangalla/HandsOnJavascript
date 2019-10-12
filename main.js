0add29025585da52c001a893f04dc56d489a4dd5
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-12 13:54:24 -0700

    while looping

--- main.js
+++ main.js
@@ -24,9 +24,18 @@ console.log(todoJSON);
 
 //For 
 
-for( let i = 0 ; i< 10;i++)
+for( let i = 0 ; i<=  10;i++)
 {
-     console.log(i);
+     console.log('For Loop Number: ${i}');
 }
 
 
+//While
+
+let i =0;
+while(i<10)
+{
+
+    console.log('While Loop Number:' ${i});
+    i++;
+}
