12f533d88e9d93c17396b1f0d78e4a1de0477b93
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-12 16:57:06 -0700

    learning Dom Manipulation

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 
 //Dom Manipulation
 
+console.log(window);
+
+
+
+
+//Single Element 
+
+//console.log(document.getElementById('my-form'));
+//console.log(document.querySelector('h1'));
+
+
+
+
+
+//Multiple Element 
+
+//console.log(document.querySelectorAll('.item'));
+
+//console.log(document.getElementsByClassName('item'));
+
+//console.log(document.getElementsByClassName('li'));
+
+
 
 
    
