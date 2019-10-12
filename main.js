90995bfa73f09c1158f31f4e82cbcd9e30d56435
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-12 15:17:27 -0700

    fatarrowfunction

--- main.js
+++ main.js
@@ -14,12 +14,21 @@ console.log(addNums(5,4));
 //Arrow Functions
 
 /*
-const addNums = (num1, num2) => {
+const addNums = (num1=1, num2=1) => {
 
     console.log(num1 + num2);
 }
 
 addNums(5,7);
+
+*/
+
+/*
+
+// fat arrow function
+const subNums = (num1, num2) => num1 - num2;
+
+console.log(subNums(10,4));
 
 */
 
