bd44e21941c4d6fd8f2f198b4bb59d4d04e3b518
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-12 15:14:03 -0700

    functions and arrow functions

--- main.js
+++ main.js
@@ -1,152 +1,27 @@
- const todos = [
-     {
-      id: 1,
-      text: 'Take out trash',
-      isCompleted: true   
-     },
-     {
-        id: 2,
-        text: 'Meeting with boss',
-        isCompleted: true   
-    },
-    {
-        id: 3,
-        text: 'Dentist appt',
-        isCompleted: false   
-    },
-    {
-        id: 4,
-        text: 'Coding',
-        isCompleted: false   
-    }
 
 
-];
-
-/*
-const todoJSON = JSON.stringify(todos);
-
-console.log(todoJSON);
-
-*/
-
-//For 
-
- /* for( let i = 0 ; i< todos.length;i++)
+//Function 
+/* function addNums(num1, num2)
 {
-     console.log(todos[i].text);
+    return num1 + num2;
 }
+
+console.log(addNums(5,4));
 
 */
 
 
-//While
+//Arrow Functions
 
 /*
-let i =0;
-while(i<10)
-{
+const addNums = (num1, num2) => {
 
-    console.log('While Loop Number:' ${i});
-    i++;
-}
-*/
-
-
-/*
-for(let todo of todos)
-{
-    console.log(todo.id);
-}
-*/
-
-
-//forEACH, map, filter 
-
-/*
-
-todos.forEach(function (todos){
-
-    console.log(todos.text);
-
-});
-
-*/
-
-//map 
-
-/*
-const todoText  = todos.map(function(todo){
-
- return todo.text;
-
-}
-);
-
-console.log(todoText);
-
-*/
-
-//filter 
-
-/*
-const todoCompleted  = todos.filter(function(todo){
-
-    return todo.isCompleted === true;
-   
-   }).map(function(todo){
-
-    return todo.text;
-   })
-   
-
-console.log(todoCompleted);
-
-*/
-// == equal doesnt match datatype condition 
-// === equal matches datatype condition 
-
-
-/*
-const x = 10;
-const y = 15;
-
-if(x > 5 && y > 10)
-{
- console.log('x is more than 5 or y is more than 10');
+    console.log(num1 + num2);
 }
 
-if( x > 5 )
-{
-    if(y>10)
-    {
+addNums(5,7);
 
-    }
-}
 */
-
-
-const x = 9;
-
-const color = x > 10 ? 'red' : 'blue';
-
-console.log(color);
-
- 
-switch(color)
-{
-    case 'red':
-        console.log('color is red');
-        break;
-    case 'blue':
-        console.log('color is blue');
-        break;
-
-    default: 
-        console.log('color is not red or blue');
-        break;
-
-}
 
 
 
