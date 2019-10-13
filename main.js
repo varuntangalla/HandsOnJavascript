6e2dde0246a7b58c61e83eca0438069469099fd5
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-12 17:31:46 -0700

    Dom Manipulation

--- main.js
+++ main.js
@@ -2,7 +2,28 @@
 
 const ul = document.querySelector('.items');
 
+//ul.remove();
+//ul.lastElementChild.remove();
 
-ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+
+ul.children[1].innerText = 'Brad';
+
+ul.lastElementChild.innerHTML = '<h4>Hello</h4>';
+
+const btn = document.querySelector('.btn');
+
+
+btn.addEventListener('mouseoit', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form')
+    .style.background ='#ccc';
+
+    document.querySelector('body').classList.add('bg-dark');
+
+    document.querySelector('.items')
+    .lastElementChild.innerHTML = '<h1>Hello</h1>'
+});
+
 
 
